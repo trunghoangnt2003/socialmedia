7f64bf25e450d81395d0d2e73d0235a9689420a0
Manhhuy15 <huya3k52@gmail.com>
2025-01-16 15:41:35 +0700

    add enums

---
+++ SocialMedia/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace SocialMedia
+{
+    public enum Types
+    {
+        POST = 1,
+        STORY = 2,
+        SHARE = 3,
+        COMMENT = 4,
+        IMAGE = 5,
+        VIDEO = 6,
+        TEXT = 7,
+    }
+    public enum Status
+    {
+        SEND = 1,
+        ISFRIENDS = 2,
+        SEEN = 3,
+    }
+
+}

23de9032c8f5b0f929e1a3ef6e59608143b8c671
kttanh <kttanh03@gmail.com>
2025-02-01 21:48:52 +0700

    done admin login page

---
+++ SocialMedia/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Models;
+
+namespace SocialMedia.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        SocialNetworkContext _context;
+
+        public AdminController(SocialNetworkContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string Email, string Password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == Email && u.Password == Password);
+
+            if (user != null) 
+            {
+                return RedirectToAction("DashBoard");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid email or password, please again!";
+                return View();
+            }
+
+        }
+    }
+}

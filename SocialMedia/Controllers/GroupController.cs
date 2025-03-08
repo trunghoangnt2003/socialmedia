bc8e86b1c96a1a59f32ca5bcd7ddab2a563015a3
kttanh <kttanh03@gmail.com>
2025-03-08 16:01:00 +0700

    save code, have bug UI

---
+++ SocialMedia/Controllers/GroupController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Models;
+
+namespace SocialMedia.Controllers
+{
+    public class GroupController : Controller
+    {
+        SocialNetworkContext _context;
+
+        public GroupController(SocialNetworkContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var group = _context.Groups
+                .FirstOrDefault(g => g.Id == id);
+
+            var countMember = _context.UserGroups.Count(g => g.Group == id);
+
+            if (group == null) return NotFound();
+
+            var posts = _context.Posts
+                .Where(p => p.Group == id)
+                .Include(p => p.AuthorNavigation)
+                .Include(p => p.Resources)
+                .Include(p => p.Comments)
+                .Include(p => p.Reactions)
+                .ToList();
+
+            ViewBag.Group = group;
+            ViewBag.Member = countMember;
+            return View(posts);
+        }
+
+    }
+}
